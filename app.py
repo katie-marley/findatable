157a74556cbe51b47feec3827db58e1b61006427
katie-marley <97759620+katie-marley@users.noreply.github.com>
2022-04-14 20:18:13 +0100

    added comment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from application import app
 # import the ./application/routes.py file
 from application import routes
 
-
+# comment added #
 if __name__ == '__main__':
      # app.run(debug=True, host='0.0.0.0')
      app.run(debug=True)
