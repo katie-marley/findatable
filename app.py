46fad5f4454ecdee44f725c077c933b2df0d7aed
katie-marley <97759620+katie-marley@users.noreply.github.com>
2022-04-15 18:50:50 +0100

    Starting off the sign up pages

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from application import app
+from flask import Flask, render_template
 # import the ./application/routes.py file
 from application import routes
 
 
+app.secret_key = 'replace later'
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+
 if __name__ == '__main__':
-     # app.run(debug=True, host='0.0.0.0')
-     app.run(debug=True)
+    # app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
