7c0589fc424f55ed47706da6ecf34907d9c157bf
katie-marley <97759620+katie-marley@users.noreply.github.com>
2022-04-18 18:54:24 +0100

    all data added

--- application/create.py
+++ application/create.py
@@ -8,8 +8,7 @@ from application.models import Restaurant, User, Cuisine, Price
 db.drop_all()
 db.create_all()
 
-# restaurant18 = Restaurant(Restaurant_Name='Wahaca Covent Garden', Address_Line_1='66 Chandos Place', City='London', Postcode='WC2N 4HG', Telephone='020 3951 9741', Opening_Hours='Monday to Sunday, 12pm to 10pm', Cuisine_ID= 6, Price_ID=2)
-# db.session.add(restaurant18)
+
 testPerson1 = User(FirstName='Julie', LastName='Dooley', PrefName='Jules', Email='JD@gmail.com', Password='password',
                     Phone='07763842259', AddressLine1='1', AddressLine2='The Hayes', City='Epsom', Postcode='KT186HB',
                     Allergens='None')
@@ -52,5 +51,7 @@ restaurant5 = Restaurant(Restaurant_Name='Umu Restaurant', Address_Line_1='14 to
 db.session.add(restaurant5)
 restaurant6 = Restaurant(Restaurant_Name='Ella Canta', Address_Line_1='1 Hambleton Place', City='London', Postcode='W1J 7QI', Telephone='02073188715', Opening_Hours='Monday to Sunday, 12pm to 12am', Cuisine_ID=6, Price_ID=3)
 db.session.add(restaurant6)
+restaurant7 = Restaurant(Restaurant_Name='Wahaca Covent Garden', Address_Line_1='66 Chandos Place', City='London', Postcode='WC2N 4HG', Telephone='020 3951 9741', Opening_Hours='Monday to Sunday, 12pm to 10pm', Cuisine_ID= 6, Price_ID=2)
+db.session.add(restaurant7)
 
 db.session.commit()
